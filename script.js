2058f21b537aa0968f45b00eb2dbcd245288a4cf
Milica <mihajlija@gmail.com>
2017-03-17 17:50:32 +0100

    Fix layout issues.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var answer1 =  "<h1 class='f6 f2-m f-subheadline-l fw8 tc b'>No.</h1>";
 var answer2 =  "<h1 class='f6 f2-m f-subheadline-l fw8 tc b'>Don't text him.</h1>";
 var img = "<img src='http://i3.kym-cdn.com/photos/images/newsfeed/001/179/536/c7f.gif'/>";
-var button =  "<a class='f6 link ph3 pv2 mb2 dib white bg-black tracked' href='#0' onclick='compute(answer2, button)'>Are you sure?</a>"
+var button =  "<a class='f4 link ph4 pv3 mb2 dib white bg-black tracked' href='#0' onclick='compute(answer2, button)'>Are you sure?</a>"
 
 
 function display(html) {
