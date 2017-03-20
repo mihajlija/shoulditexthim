813e1855fdc914ee01a341ae13d4a64eb431481a
Milica <mihajlija@gmail.com>
2017-03-20 01:51:32 +0100

    Fix onclick bug

--- script.js
+++ script.js
@@ -1,55 +1,63 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
 
-            var answer1 = "<h1 class='f6 f2-m f-subheadline-l fw8 tc b'>No.</h1>";
-            var answer2 = "<h1 class='f6 f2-m f-subheadline-l fw8 tc b'>Don't text him.</h1>";
-            var img = "<img src='http://i3.kym-cdn.com/photos/images/newsfeed/001/179/536/c7f.gif'/>";
-            var button = "<a class='f4 link ph4 pv3 mb2 dib white bg-black tracked' href='#0' onclick='compute(answer2, button)'>Are you sure?</a>";
-            var aboutBtnOpen = document.getElementById("js-about-open");
-            var aboutBtnClose = document.getElementById("js-about-close")
-            var aboutPage = document.getElementById("js-about-page");
+ var answer1 = "<h1 class='f6 f2-m f-subheadline-l fw8 tc b'>No.</h1>";
+ var answer2 = "<h1 class='f6 f2-m f-subheadline-l fw8 tc b'>Don't text him.</h1>";
+ var img = "<img src='http://i3.kym-cdn.com/photos/images/newsfeed/001/179/536/c7f.gif'/>";
+ var button = "<a class='f4 link ph4 pv3 mb2 dib white bg-black tracked' href='#0' onclick='compute(answer2, button)'>Are you sure?</a>";
 
+ function compute(answer, button) {
+     console.log("test");
+     display(img);
+     thinkAboutIt(answer + button);
+ }
 
-            function display(html) {
-                document.getElementById("main").innerHTML = html;
-            }
+ function display(html) {
+     document.getElementById("main").innerHTML = html;
+ }
 
-            function getRandom(min, max) {
-                return Math.random() * (max - min) + min;
-            }
-
-
-            function thinkAboutIt(answer) {
-                milliseconds = getRandom(1000, 8000);
-                setTimeout(function () {
-                    display(answer)
-                }, milliseconds);
-            }
-
-            function compute(answer, button) {
-                display(img);
-                thinkAboutIt(answer + button);
-            }
-
-            aboutBtnOpen.addEventListener('click', function () {
-                aboutPage.classList.add("open");
-            }, false);
-
-            aboutBtnClose.addEventListener('click', function () {
-                aboutPage.classList.remove("open");
-            }, false);
+ function getRandom(min, max) {
+     return Math.random() * (max - min) + min;
+ }
 
 
-            /*
-            function shouldITextHim(){
-                document.getElementById('unsure').onclick = function() {
-                  var unsure = true;
-                }​;​
+ function thinkAboutIt(answer) {
+     milliseconds = getRandom(1000, 8000);
+     setTimeout(function () {
+         display(answer)
+     }, milliseconds);
+ }
 
-                if (unsure == true)
-                   var fuccboi = true;
-                if (fuccboi == true)
-                     var answer = false;
-                return answer;     
-            }
-            */
-        });
+
+ /*
+ function shouldITextHim(){
+     document.getElementById('unsure').onclick = function() {
+       var unsure = true;
+     }​;​
+
+     if (unsure == true)
+        var fuccboi = true;
+     if (fuccboi == true)
+          var answer = false;
+     return answer;     
+ }
+ */
+
+ document.addEventListener("DOMContentLoaded", function (event) {
+
+
+     var aboutBtnOpen = document.getElementById("js-about-open");
+     var aboutBtnClose = document.getElementById("js-about-close")
+     var aboutPage = document.getElementById("js-about-page");
+
+
+
+
+
+     aboutBtnOpen.addEventListener('click', function () {
+         aboutPage.classList.add("open");
+     }, false);
+
+     aboutBtnClose.addEventListener('click', function () {
+         aboutPage.classList.remove("open");
+     }, false);
+
+ });
