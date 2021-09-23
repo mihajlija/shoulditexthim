1b679b6e2392c5111204b1e89b3f70892505cf4c
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2021-09-23 14:23:14 +0300

    Fix Loading
    Old icon had 404 error. That caused a broken image while loading which may lead to confuse the user to text him.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 var answer1 =  "<h1 class='f6 f2-m f-subheadline-l fw8 tc b'>No.</h1>";
 var answer2 =  "<h1 class='f6 f2-m f-subheadline-l fw8 tc b'>Don't text him.</h1>";
-var img = "<img src='http://i3.kym-cdn.com/photos/images/newsfeed/001/179/536/c7f.gif'/>";
+var img = "<svg viewBox='0 0 100 100' height='200px'><path fill='#000' d='M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50'><animateTransform attributeName='transform' type='rotate' repeatCount='indefinite' dur='1s' from='0 50 50' to='360 50 50' /></path></svg>";
 var button =  "<a class='f6 link ph3 pv2 mb2 dib white bg-black tracked' href='#0' onclick='compute(answer2, button)'>Are you sure?</a>"
 
 
