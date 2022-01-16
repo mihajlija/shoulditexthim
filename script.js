225739b02fc86c0d1b9a325ff6ad041101c10704
Milica <mihajlija@google.com>
2022-01-16 18:56:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 var answer1 =  "<h1 class='f6 f2-m f-subheadline-l fw8 tc b'>No.</h1>";
 var answer2 =  "<h1 class='f6 f2-m f-subheadline-l fw8 tc b'>Don't text him.</h1>";
-var img = "<img src='http://i3.kym-cdn.com/photos/images/newsfeed/001/179/536/c7f.gif'/>";
+var img = "<img src='math-lady.gif'/>";
 var button =  "<a class='f6 link ph3 pv2 mb2 dib white bg-black tracked' href='#0' onclick='compute(answer2, button)'>Are you sure?</a>"
 
 
